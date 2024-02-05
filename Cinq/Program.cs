f6b7bb5379abe4d1526c00cced58fd22f69286cd
VonRedNode <vonrednode@gmail.com>
2024-02-05 10:54:24 +0100

    Cinq but better

--- Cinq/Program.cs
+++ Cinq/Program.cs
@@ -58,17 +58,62 @@ namespace Cinq
                 { 1,1,1,1,1,1,1,1,1,1 },
                 { 2,1,1,1,1,1,1,1,1,1 },
             };
-
-            Console.WriteLine(field);
-            Console.WriteLine(field.GetLength(0));
-            Console.WriteLine(field.GetLength(1));
-
-            Console.WriteLine(field[0,0]);
-
             drawField(field);
 
+            double speedInSand = 5.0;
+            double speedInWater = 2.5;
+
+            double submergingLine = 50.0;
+            double b = 50.0;
+            double e = 50.0;
+
+            double shortestTime = 0.0;
+            double shortestPath = 0.0;
+            
+            
+            while(submergingLine <= 100)  
+            {
+                double a = submergingLine;
+                double d = 100.0 - submergingLine;
+
+                double distanceToRun = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+                double distanceToSwim = Math.Sqrt(Math.Pow(d, 2) + Math.Pow(e, 2));
+                double distanceTotal = distanceToRun + distanceToSwim;
+
+                double timeToRun = distanceToSubmergingLine/speedInSand;
+                double timeToSwim = distanceToDrowningDwarf/speedInWater;
+                double timeTotal = timeToRun + timeToSwim;
+
+
+                if(timeTotal > shortestTime) 
+                {
+                    shortestTime = timeTotal
+                    shortestPath = distanceTotal
+                }
+
+                submergingLine += 0.01;
+            }
+
+
+            Console.WriteLine("Congrats, you decided to save the poor dwarf drowning");
+            Console.WriteLine($"It took you a total of \t{shortestTime} seconds");
+            Console.WriteLine($"It took you \t{shortestPath} meters");
 
             Console.Read();
+            /*
+            double sandC = Math.Sqrt(Math.Pow(sandA,2) + Math.Pow(sandB,2))
+            double waterC =  Math.Sqrt(Math.Pow(sandA,2) + Math.Pow(sandB,2))
+
+            double secondsTakenForSand = sandC/speedInSand
+            double secondsTakenForWater = waterC/speedInWater
+            double secondsTakenTotal = secondsTakenForSand + secondsTakenForWater
+
+
+            Console.WriteLine("Congrats, you decided to save the poor dwarf drowning")
+            Console.WriteLine($"It took you a total of \t{secondsTakenTotal} seconds")
+            Console.WriteLine($"It took you \t{secondsTakenForSand} seconds in sand")
+            Console.WriteLine($"It took you \t{secondsTakenForWater} seconds in water")
+            */
         }
     }
 }
